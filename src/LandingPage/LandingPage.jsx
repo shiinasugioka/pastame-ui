4a562d4f1520446ef282fa1043a1d7dd5f7a1e5c
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-10-14 18:03:56 -0700

    fix conflicts

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,23 +1,41 @@
-import React from "react";
 import { useState } from "react";
+import axios from "axios";
 import "./LandingPage.css";
 
 function LandingPage() {
   const [count, setCount] = useState(0);
+  const [file, setFile] = useState(null);
 
+  const onFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const onUpload = async () => {
+    const formData = new FormData();
+    formData.append("imgfile", file);
+
+    try {
+      const response = await axios.post("http://localhost:8080/upload", formData);
+      console.log("Response:", response.data);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <div>
+          <input type="file" id="imgfile" onChange={onFileChange} />
+          <button id="submit-btn" onClick={onUpload}>
+            Start Cooking
+          </button>
+        </div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
