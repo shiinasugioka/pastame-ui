576430166492191d67393fda3ce39fed01554c8b
Audrey Kho <audreyckho@gmail.com>
2023-10-14 21:26:29 -0700

    added recipe endpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/recipes", async (req, res) => {
+  try {
+    const recipes = await axios.get(
+      `${process.env.RECIPE_API_URL}?q=pasta,${req.query.ingredients}&app_id=${process.env.RECIPE_APP_ID}&app_key=${process.env.RECIPE_API_KEY}`
+    );
+
+    console.log(recipes.data);
+    res.send(recipes.data.hits);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
